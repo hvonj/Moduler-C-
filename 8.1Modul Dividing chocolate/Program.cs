63c936f0f288c21bc83b1347f3f8de16d2a6b4bc
Heidivonjahf <heidivonjahf@hotmail.com>
2018-12-03 16:53:52 +0100

    Modul 8.1

---
+++ 8.1Modul Dividing chocolate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Uppg_8_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            decimal persons = AskForPersons();
+            ReturnHowMany(persons);
+        }
+
+
+        static decimal AskForPersons()
+        {
+
+            Console.Write("The chocolatebar contains 24 pieces. \n" +
+                "How many want to share? ");
+            try
+            {
+                decimal persons = decimal.Parse((Console.ReadLine()));
+                Console.WriteLine();
+                return persons;
+            }
+
+
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You must enter a number!");
+                Console.ResetColor();
+                return AskForPersons();
+            }
+
+        }
+
+        private static void ReturnHowMany(decimal persons)
+        {
+            try
+            {
+                decimal each = (24 / persons);
+
+                Console.WriteLine($"Everyone gets {each} pieces. \n");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Zero People cant divide a chocolate!");
+                Console.ResetColor();
+            }
+
+        }
+
+    }
+}
