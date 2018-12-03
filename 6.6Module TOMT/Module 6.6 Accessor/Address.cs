1396e030677e5cfdfbb7005a2294f759c5bf4c1e
Heidivonjahf <heidivonjahf@hotmail.com>
2018-12-03 14:51:03 +0100

    Nytt i 6.6

---
+++ 6.6Module TOMT/Module 6.6 Accessor/Address.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accessor
+{
+    class Address
+    {
+        public String Street { get; set; }
+        public int StreetNumber { get; set; }
+        public String City { get; set; }
+
+        private string zipCode; // 111 22
+
+        public void SetZipCode(string inputZip)
+        {
+           string cleanInputZip = inputZip.Trim();
+            int numbers;
+            string nospacees = zipCode.Replace(" ", "");
+            if (int.TryParse(nospacees, out numbers ))
+            {
+                zipCode = inputZip;
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wrong format try XX XXX! ");
+                Console.ResetColor();
+                zipCode = zipCode;
+
+                if (zipCode.Equals("Exit"))
+                {
+                    zipCode = zipCode;
+                }
+                else
+                {
+                    Console.WriteLine("Gör igen!");
+                }
+            }  
+        }
+        
+        public string GetZipCode()
+        {
+            return zipCode;
+        }
+
+
+
+        public string GetFullStreet()
+        {
+           return Street+ " " + StreetNumber;
+        }
+        
+        public string FullStreet => Street + " " + StreetNumber;
+     
+    }
+}
---
+++ 6.6Module TOMT/Module 6.6 Accessor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Accessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address firstAdress = new Address
+            {
+                Street = "Första Gatan",
+                StreetNumber = 12,
+                City = "Göteborg",
+                ZipCode = 1234,
+            };
+            Console.WriteLine($"Street: \t \t {firstAdress.Street} \n" +
+                $"StreetNumber: \t \t {firstAdress.StreetNumber} \n" +
+                $"City: \t \t \t {firstAdress.City} \n" +
+                $"ZipCode: \t \t {firstAdress.ZipCode} \n" +
+                $"FullStreet: \t \t {firstAdress.FullStreet}");
+        }
+    }
+}
